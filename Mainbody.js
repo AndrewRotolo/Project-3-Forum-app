d581afd4244115adf40ffcad80fdc8c82d8438cd
Christian Shelton <christianshelton@christians-mbp.lan>
2023-06-12 23:56:45 -0400

    updated

---
+++ Mainbody.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default function MainBody() {
+
+    //don't forget to properly hook this up with the DB later. This is just for styling purposes
+    const posts = ['placeholder1', 'placeholder2', 'placeholder3']
+
+    return (
+        <div>
+        <div>
+            <button id='newPost'>Create New Post</button>
+        </div>
+
+        <ul id='postList'>
+            {posts.map((post) => (
+                <li className='posts'>{post}</li>
+            ))}
+        </ul>
+
+        </div>
+    )
+}
